222a158349e15e0c6777c171358d6010764a1c31
boyratata <animeboylolaaa@gmail.com>
2024-06-11 04:12:06 +0300

    Initial commit with website files

---
+++ _next/static/chunks/app/page-60d615703e2370a8.js
@@ -0,0 +1,280 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[931], {
+    2431: function() {},
+    7624: function(e, t, l) {
+        Promise.resolve().then(l.bind(l, 2302))
+    },
+    2302: function(e, t, l) {
+        "use strict";
+        l.r(t),
+        l.d(t, {
+            default: function() {
+                return f
+            }
+        });
+        var a, n, i, r = l(7437), s = l(2846);
+        function o() {
+            return (o = Object.assign ? Object.assign.bind() : function(e) {
+                for (var t = 1; t < arguments.length; t++) {
+                    var l = arguments[t];
+                    for (var a in l)
+                        Object.prototype.hasOwnProperty.call(l, a) && (e[a] = l[a])
+                }
+                return e
+            }
+            ).apply(this, arguments)
+        }
+        var c = function(e) {
+            return s.createElement("svg", o({
+                xmlns: "http://www.w3.org/2000/svg",
+                xmlnsXlink: "http://www.w3.org/1999/xlink",
+                viewBox: "0 0 906.33 362"
+            }, e), a || (a = s.createElement("defs", null, s.createElement("linearGradient", {
+                id: "logo-text-white_svg__linear-gradient",
+                x1: 254.32,
+                x2: 157.31,
+                y1: 26.19,
+                y2: 311.16,
+                gradientUnits: "userSpaceOnUse"
+            }, s.createElement("stop", {
+                offset: 0,
+                stopColor: "#27aae1"
+            }), s.createElement("stop", {
+                offset: 1,
+                stopColor: "#2b3990"
+            })), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-2",
+                x1: 357.47,
+                x2: -151.27,
+                y1: 275.77,
+                y2: 403.1
+            }), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-3",
+                x1: 66.8,
+                x2: 486.29,
+                y1: 343.43,
+                y2: 264.99
+            }), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-4",
+                x1: 12.09,
+                x2: 110.43,
+                y1: 94.24,
+                y2: 375.61
+            }), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-5",
+                x1: 68.26,
+                x2: 9.71,
+                y1: 317.57,
+                y2: 56.09
+            }), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-6",
+                x1: 172.91,
+                x2: 52.79,
+                y1: 282.15,
+                y2: 129.44
+            }), s.createElement("linearGradient", {
+                xlinkHref: "#logo-text-white_svg__linear-gradient",
+                id: "logo-text-white_svg__linear-gradient-7",
+                x1: 35.99,
+                x2: 200.08,
+                y1: 152.18,
+                y2: 344.31
+            }), s.createElement("style", null, ".logo-text-white_svg__cls-8{fill:#fff}"))), s.createElement("g", {
+                id: "logo-text-white_svg__Layer_2",
+                "data-name": "Layer 2"
+            }, s.createElement("g", {
+                id: "logo-text-white_svg__Layer_3",
+                "data-name": "Layer 3"
+            }, s.createElement("g", {
+                id: "logo-text-white_svg__Layer_4",
+                "data-name": "Layer 4"
+            }, s.createElement("path", {
+                d: "M362 181q0 7.92-.67 15.66c-5.55-17.39-19.61-44.32-53.48-44.32-50 0-54.19 44.6-54.19 44.6a22 22 0 0 1 18.19-9c12.51 0 19.71 4.92 19.71 18.19s-11.75 23.11-31.45 23.11-48.44-11.37-48.44-42.44c0-58.71 52-62.77 52-62.77-28.8-9.58-54.69 8.51-68.21 27.05C170.35 185.6 190 219 190 219s-33.87-18.95-33.87-74.24c0-89.28 91.33-100.93 125.58-87.19-23.71-23.71-43.4-29.5-69.12-29.5-62.52 0-108.22 60.14-108.22 111 0 44.3 34.85 117.16 132.31 117.16 86.66 0 95.46-55.1 86-69 36.54 36.57-17.83 84.13-86 84.13-28.87 0-105.17-6.56-150.89-79.6C19.85 86.4 146.42 15.92 146.42 15.92 113.5 13.7 91.6 23.77 89.8 24.63A181 181 0 0 1 362 181",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient)"
+                }
+            }), s.createElement("path", {
+                d: "M349.39 247.5a181 181 0 0 1-300 57.7 361.8 361.8 0 0 0 88.05 10.8q9.66 0 19.2-.5 15.56-.81 30.75-2.92a360.06 360.06 0 0 0 162-65.08",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-2)"
+                }
+            }), s.createElement("path", {
+                d: "M349.39 247.5a181 181 0 0 1-271.9 82 363.8 363.8 0 0 0 65.29 1.25q9.63-.68 19.12-1.85 15.45-1.9 30.46-5.07a360 360 0 0 0 157.03-76.33",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-3)"
+                }
+            }), s.createElement("path", {
+                d: "M115.23 296.61a362 362 0 0 1-37.1-4.17 360 360 0 0 1-47.93-11.32A180.1 180.1 0 0 1 0 183.34v-3.49A180.4 180.4 0 0 1 51.63 55.64l.12-.13v.05a1 1 0 0 0-.09.15q-1.06 1.93-2.07 3.9c-.39.74-.77 1.49-1.14 2.24-.28.55-.56 1.11-.83 1.67q-2.22 4.54-4.18 9.24a97.2 97.2 0 0 0-7.27 20.86 178 178 0 0 0-2.35 9.29c-.19.85-.37 1.7-.55 2.56-.45 2.17-.87 4.35-1.24 6.55-.15.84-.28 1.68-.41 2.53q-.24 1.53-.45 3.06c-.12.85-.23 1.71-.33 2.56-.05.33-.09.67-.12 1-.09.67-.17 1.34-.23 2 0 .37-.08.73-.12 1.1q-.13 1.31-.24 2.61c-.18 2.08-.33 4.16-.43 6.26-.06 1-.1 2.1-.14 3.15s-.06 2.11-.08 3.16 0 2.12 0 3.18a180.89 180.89 0 0 0 85.75 153.98",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-4)"
+                }
+            }), s.createElement("path", {
+                d: "M78.13 292.44a360 360 0 0 1-47.93-11.32A180.1 180.1 0 0 1 0 183.34v-3.49A180.4 180.4 0 0 1 51.63 55.64l.1-.08.05-.05-.14.2q-1.26 1.8-2.46 3.65c-.46.69-.92 1.4-1.37 2.1l-1 1.58c-1.79 2.85-3.51 5.77-5.13 8.74a97 97 0 0 0-9.44 20q-1.77 4.42-3.32 9c-.28.82-.55 1.65-.82 2.48-.67 2.11-1.32 4.24-1.92 6.38-.24.82-.46 1.65-.68 2.48-.27 1-.52 2-.77 3-.21.83-.41 1.68-.6 2.51-.08.33-.16.66-.22 1-.16.66-.32 1.32-.45 2-.08.36-.15.72-.23 1.08-.18.86-.35 1.71-.52 2.57-.4 2-.76 4.1-1.09 6.18-.17 1-.32 2.08-.47 3.12s-.28 2.09-.41 3.13-.26 2.11-.37 3.16a180.73 180.73 0 0 0 57.76 152.57",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-5)"
+                }
+            }), n || (n = s.createElement("path", {
+                d: "M236.69 283.5a361.5 361.5 0 0 1-48.61 10.27 365 365 0 0 1-45.85 3.48 178 178 0 0 1-18.77-10.77 180.75 180.75 0 0 1-77.22-148.31q0-7.12.55-14.12a180.91 180.91 0 0 0 179.76 159.74c3.45 0 6.79-.09 10.14-.29"
+            })), s.createElement("path", {
+                d: "M236.69 283.5a361.5 361.5 0 0 1-48.61 10.27 365 365 0 0 1-45.85 3.48 178 178 0 0 1-18.77-10.77 180.75 180.75 0 0 1-77.22-148.31q0-7.12.55-14.12a180.91 180.91 0 0 0 179.76 159.74c3.45 0 6.79-.09 10.14-.29",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-6)"
+                }
+            }), s.createElement("path", {
+                d: "M188.08 293.77a365 365 0 0 1-45.85 3.48 178 178 0 0 1-18.77-10.77 180.75 180.75 0 0 1-77.22-148.31q0-7.12.55-14.12a180.94 180.94 0 0 0 141.29 169.72",
+                style: {
+                    fill: "url(#logo-text-white_svg__linear-gradient-7)"
+                }
+            }), i || (i = s.createElement("path", {
+                d: "m543.87 193.45 17-60.43a5.31 5.31 0 0 1 5.12-3.88h25.22l-33.4 103.72h-26.39l-19.5-62-19.5 62h-26.34l-33.4-103.72h25.21A5.32 5.32 0 0 1 463 133l17 60.43 19.5-64.31h24.89ZM665.85 141.17v-6.71a5.32 5.32 0 0 1 5.32-5.32h22.28v103.72h-22.28a5.32 5.32 0 0 1-5.32-5.32v-6.92C657.14 232 642.62 236 633.08 236c-30.91 0-51.25-23.24-51.45-55 .2-31.74 20.54-55 51.45-55 9.54 0 24.27 3.76 32.77 15.17m2.49 39.83c0-20.95-15.35-30.08-29.25-30.08-14.1 0-29.87 8.51-30.08 30.08.21 21.58 16 30.08 30.08 30.08 13.91 0 29.25-9.08 29.25-30.08M696.51 129.14h26.54a5.32 5.32 0 0 1 5 3.52l23.63 66 23.63-66a5.33 5.33 0 0 1 5-3.52h26.54l-41.67 103.72h-27ZM897.15 156.62a61 61 0 0 1 3.42 8l-68 34.8c8.68 15.13 23 15.74 33.87 10.16 6.88-3.53 12.28-9.57 14.34-16.64a5.3 5.3 0 0 1 2.68-3.27l22.76-11.65c1 26.06-9.9 41.66-29.47 51.68-27 13.81-56.73 5.28-72.15-24.82s-4.93-59.23 22-73c27.78-14.18 56.65-2.4 70.55 24.74m-72.77 25.85 43.4-22.22c-8.48-13.38-23.59-11.47-31.16-7.59-8.49 4.34-17.21 14.64-12.24 29.81",
+                className: "logo-text-white_svg__cls-8"
+            }))))))
+        }
+          , d = l(1399);
+        function x() {
+            let e = e=>{
+                let t = window.open(e, "_blank", "noopener,noreferrer");
+                t && (t.opener = null)
+            }
+            ;
+            return (0,
+            r.jsxs)("div", {
+                className: "flex flex-row w-full text-white py-3 navbar px-5 md:px-0",
+                children: [(0,
+                r.jsx)("span", {
+                    children: (0,
+                    r.jsx)(c, {
+                        width: 80,
+                        height: 40
+                    })
+                }), (0,
+                r.jsxs)("div", {
+                    className: "hidden lg:flex w-full justify-left ml-[3em] gap-[3em] text-center items-center font-normal select-none",
+                    children: [(0,
+                    r.jsx)("a", {
+                        className: "active",
+                        children: "Home"
+                    }), (0,
+                    r.jsx)("a", {
+                        className: "!cursor-not-allowed",
+                        children: "Features"
+                    }), (0,
+                    r.jsx)("a", {
+                        className: "!cursor-not-allowed",
+                        children: "Pricing"
+                    }), (0,
+                    r.jsx)("a", {
+                        className: "!cursor-not-allowed",
+                        children: "Resources"
+                    })]
+                }), (0,
+                r.jsx)("div", {
+                    className: "w-full xl:w-[15%] flex justify-end",
+                    children: (0,
+                    r.jsx)(d.j2d, {
+                        size: 35,
+                        className: "cursor-pointer transition duration-300 ease-in-out brightness-75 hover:brightness-125",
+                        onClick: ()=>e("https://discord.getwave.gg")
+                    })
+                })]
+            })
+        }
+        l(7134);
+        var h = l(2362)
+          , g = l(703);
+        function f() {
+            return (0,
+            r.jsxs)("div", {
+                className: "w-full h-screen overflow-clip relative",
+                children: [(0,
+                r.jsx)("div", {
+                    className: "absolute h-[145%] w-[120%] -top-[22em] -left-10 -z-10 overlay",
+                    children: (0,
+                    r.jsx)("div", {
+                        className: "w-full h-full bg-[url('/images/background-landing.jpg')] bg-no-repeat bg-cover bg-top blur-xl"
+                    })
+                }), (0,
+                r.jsx)("div", {
+                    className: "container relative mx-auto w-full h-full py-6 overflow-hidden",
+                    children: (0,
+                    r.jsxs)("div", {
+                        className: "h-full w-full flex flex-col z-1",
+                        children: [(0,
+                        r.jsx)(x, {}), (0,
+                        r.jsxs)("div", {
+                            className: "flex flex-row w-full h-full gap-7",
+                            children: [(0,
+                            r.jsxs)("div", {
+                                className: "flex flex-col w-full xl:w-1/2 text-white gap-4 text-center xl:text-left",
+                                children: [(0,
+                                r.jsx)("span", {
+                                    className: "text-[5em] xl:text-[7em] font-semibold mt-[20%] xl:mt-[30%] leading-[1.1em] glow-blue-text",
+                                    children: "LET THE SONG BEGIN"
+                                }), (0,
+                                r.jsx)("span", {
+                                    className: "text-lg",
+                                    children: "Experience Song-Advanced, Embark on a harmonious journey with Song Advance, where Python commands the spotlight as the supreme scripting tool on the grand stage of music. Sense the beat of innovation pulsating through each line of code as you delve into its evolution, not just as an onlooker, but as a pivotal figure riding the crest of creativity. With its advanced scripting capabilities and unmatched performance, Song Advance calls upon you to plunge deep, embracing its symphonic potentials entirely. Join the ensemble of visionaries and surf the wave of the musical future with Python as your esteemed conductor."
+                                }), (0,
+                                r.jsxs)("div", {
+                                    className: "flex flex-row gap-4 justify-center xl:justify-start",
+                                    children: [(0,
+                                    r.jsx)("a", {
+                                        className: "button-main !text-black !bg-white",
+                                        onClick: ()=>window.open("https://link-center.net/1138912/dive-in-ride-the-wave", "_blank", "noopener,noreferrer"),
+                                        children: "Download"
+                                    }), (0,
+                                    r.jsx)("a", {
+                                        className: "button-main",
+                                        onClick: ()=>window.open("https://discord.getwave.gg", "_blank", "noopener,noreferrer"),
+                                        children: "Discord"
+                                    })]
+                                }), (0,
+                                r.jsxs)("div", {
+                                    className: "mt-10 flex flex-col gap-1",
+                                    children: [(0,
+                                    r.jsx)("span", {
+                                        children: "Available on:"
+                                    }), (0,
+                                    r.jsxs)("div", {
+                                        className: "flex flex-row gap-5 xl:justify-start justify-center",
+                                        children: [(0,
+                                        r.jsxs)("div", {
+                                            className: "icon",
+                                            children: [(0,
+                                            r.jsx)(d.zTP, {
+                                                size: 26
+                                            }), (0,
+                                            r.jsx)("span", {
+                                                children: "Windows 10 & 11"
+                                            })]
+                                        })]
+                                    })]
+                                })]
+                            }), (0,
+                            r.jsx)("div", {
+                                className: "flex-col w-1/2 hidden xl:flex"
+                            }), (0,
+                            r.jsx)(g.default, {
+                                src: "/images/test.jpg",
+                                width: 720,
+                                height: 500,
+                                alt: "Picture of the author",
+                                className: "absolute right-0 top-1/3 -z-[1] backdrop-blur hidden xl:flex"
+                            })]
+                        })]
+                    })
+                })]
+            })
+        }
+    }
+}, function(e) {
+    e.O(0, [240, 699, 974, 971, 69, 744], function() {
+        return e(e.s = 7624)
+    }),
+    _N_E = e.O()
+}
+]);
